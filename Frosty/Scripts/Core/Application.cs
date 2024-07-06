e66ccf310bd527861acd2c4cd6c1ffeb02ceeae7
Annas <annaskherunas06@gmail.com>
2024-07-06 22:08:59 +0700

    init

---
+++ Frosty/Scripts/Core/Application.cs
@@ -0,0 +1,26 @@
+﻿using Foster.Framework;
+
+namespace Frosty.Scripts.Core;
+
+public class Application : Module
+{
+    public override void Startup()
+    {
+
+    }
+
+    public override void Update()
+    {
+
+    }
+
+    public override void Render()
+    {
+
+    }
+
+    public override void Shutdown()
+    {
+
+    }
+}
---
+++ Frosty/Scripts/Program.cs
@@ -0,0 +1,13 @@
+﻿using Foster.Framework;
+using Frosty.Scripts.Core;
+
+namespace Frosty.Scripts;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        App.Register<Application>();
+        App.Run("Frosty", 960, 540);
+    }
+}
